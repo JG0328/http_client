b29c8130ab6b8874f38b42b3f1192fb4b68db99c
José Gabriel González <jgg032899@gmail.com>
2019-09-25 17:53:58 -0400

    Agregando la entrada de la URL mediante la consola

--- src/main/java/edu/pucmm/httpclient/Main.java
+++ src/main/java/edu/pucmm/httpclient/Main.java
@@ -1,7 +1,12 @@
 package edu.pucmm.httpclient;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite una URL valida: ");
+        String myUrl = scanner.nextLine();
+        System.out.println("La URL es: " + myUrl);
     }
 }
