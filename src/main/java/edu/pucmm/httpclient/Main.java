d2f87b7a1551f6e3aa7ecc10258fe52c38922135
José Gabriel González <jgg032899@gmail.com>
2019-09-25 18:45:33 -0400

    Agregando cantidad de parrafos e imagenes

--- src/main/java/edu/pucmm/httpclient/Main.java
+++ src/main/java/edu/pucmm/httpclient/Main.java
@@ -1,5 +1,12 @@
 package edu.pucmm.httpclient;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,5 +15,23 @@ public class Main {
         System.out.println("Digite una URL valida: ");
         String myUrl = scanner.nextLine();
         System.out.println("La URL es: " + myUrl);
+
+        try {
+            Document doc = Jsoup.connect(myUrl).get();
+
+            // Contar la cantidad de lineas
+
+            // Contar la cantidad de parrafos
+            Elements ps = doc.select("p");
+            System.out.println("Cantidad de parrafos: " + ps.size());
+
+            // Contar la cantidad de imagenes dentro de parrafos
+            Elements imgs = doc.select("p").select("img");
+            System.out.println("Cantidad de imagenes dentro de parrafos: " + imgs.size());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
